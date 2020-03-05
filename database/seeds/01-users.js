070a6b894820d0ee8a319cfee6757cc13513ee02
tim-jeffries <timothyrexjeffries@gmail.com>
2020-03-04 19:32:09 -0800

    Fix seed hash

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -5,8 +6,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'IndiWhip', password: 'password1', email: 'dr.jones@marshall.edu', name: 'Indiana Jones', field_of_study: 'archaeology', occupation: 'professor'},
-        {id: 2, username: 'sheiksneak', password: 'password1', email: 'princesszelda@hyrule.gov', name: 'Princess Zelda', field_of_study: 'espionage', occupation: 'Ruler of Hyrule'}
+        {id: 1, username: 'IndiWhip', password: bcrypt.hashSync('password1', 8), email: 'dr.jones@marshall.edu', name: 'Indiana Jones', field_of_study: 'archaeology', occupation: 'professor'},
+        {id: 2, username: 'sheiksneak', password: bcrypt.hashSync('password1', 8), email: 'princesszelda@hyrule.gov', name: 'Princess Zelda', field_of_study: 'espionage', occupation: 'Ruler of Hyrule'}
       ]);
     });
 };
